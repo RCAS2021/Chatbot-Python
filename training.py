66a25f16bf56f5ee11d96f0e85f790da64b1f550
Rafael Cruz <rcas2009@outlook.com>
2024-04-08 13:12:51 -0300

    Fixing error

--- training.py
+++ training.py
@@ -95,7 +95,7 @@ model.add(tf.keras.layers.Dense(len(train_y[0]), activation='softmax'))
 sgd = tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
 model.compile(loss="categorical_crossentropy", optimizer=sgd, metrics=["accuracy"])
 
-model.fit(train_x, train_y, epochs=200, batch_size=5, verbose=1)
-model.save('chatbot_model.h5')
+hist = model.fit(train_x, train_y, epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.h5', hist)
 
 print("Done")
